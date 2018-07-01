cbd6a4e1c83be2c2a32fb035fe12fcddcc9a6f83
mungkonmoto <mungkonmoto@gmail.com>
2018-07-01 12:57:05 +0700

    edit port

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+var port = process.env.port || 3000;
+server.listen(port, () => {
  console.log("JSON Server is running");
 });
